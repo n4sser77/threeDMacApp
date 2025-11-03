d7e93fa35f7609815d86a91fa2e1d663305275bf
Naser Al-Asbahi <nasserqahtan0@gmail.com>
2025-11-03 16:12:51 +0100

    added three.js and helper libs for 3d rendering in react, renderd first macbook 14 model with some lights

--- macbookAppGui/src/components/ProductViewer.tsx
+++ macbookAppGui/src/components/ProductViewer.tsx
@@ -1,4 +1,7 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import useMacBookViewer from "../hooks/useMacBookViewer";
+import { MacBookModel14 } from "./models/Macbook-14";
 // import useMacBookStore from "../store";
 
 const ProductViewer = () => {
@@ -8,7 +11,9 @@ const ProductViewer = () => {
       <h2>Take a closer look.</h2>
 
       <div className="controls ">
-        <p className="info">MacBookPro 16" in silver / Space black</p>
+        <p className="info">
+          MacBookPro {scale} in {color}
+        </p>
 
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
@@ -23,16 +28,36 @@ const ProductViewer = () => {
           </div>
 
           <div className="size-control">
-            <div>
+            <div
+              onClick={() => setScale(0.06)}
+              className={`${
+                scale === 0.06
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              }`}
+            >
               <p>14"</p>
             </div>
-            <div>
+            <div
+              onClick={() => setScale(0.08)}
+              className={`${
+                scale === 0.08
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              }`}
+            >
               <p>16"</p>
             </div>
           </div>
         </div>
       </div>
       <p className="text-white text-4xl">Render Canvas</p>
+      <Canvas id="canvas" camera={{position: [0,2,5], fov:50, near:0.1, far:100}}>
+         <ambientLight intensity={10}/>
+        {/* <Box position={[0, 0, 0]} scale={10 * scale} material-color={color}/> */}
+        <MacBookModel14 scale={0.06} position={0,0,0} />
+        <OrbitControls enableZoom={false}/>
+      </Canvas>
     </section>
   );
 };
